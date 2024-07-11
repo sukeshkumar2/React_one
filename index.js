b1f8b7f4970e6573b8c1694b13a92dd24b6b3157
sukeshkumar2 <dosapatisukesh@gmail.com>
2024-07-11 15:59:27 -0700

    basic react code

---
+++ index.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("div",{
+    id : "parent"
+},[React.createElement("div",{
+    id : "child"
+},
+[React.createElement("h1",{id : "heading"},"Hello World from React Child 1"),React.createElement("h1",{id : "heading2"},"heading 2 from React Child 1")]),
+React.createElement("div",{
+    id : "child2"
+},
+[React.createElement("h1",{id : "heading"},"Hello World from React Child 2"),React.createElement("h1",{id : "heading2"},"heading 2 from React Child 2")])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
