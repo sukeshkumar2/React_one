d66730f4bd7fa5728fbf0558295f1872b0c49eb7
sukeshkumar2 <dosapatisukesh@gmail.com>
2024-07-14 00:51:31 -0700

    Functional Component

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div",{
-    id : "parent"
-},[React.createElement("div",{
-    id : "child"
-},
-[React.createElement("h1",{id : "heading"},"Hello World from React Child 1"),React.createElement("h1",{id : "heading2"},"heading 2 from React Child 1")]),
-React.createElement("div",{
-    id : "child2"
-},
-[React.createElement("h1",{id : "heading"},"Hello World from React Child 2"),React.createElement("h1",{id : "heading2"},"heading 2 from React Child 2")])]);
+const Title = ()=>{
+    return (
+    <h1 className="title" tabIndex="5">
+        This is Component one title
+    </h1>
+)}
 
+const HeadingComponent = ()=>(
+    <div className="container">
+        <Title/>
+        <h1 className="heading" tabIndex="5">
+        This is heading in component 2
+    </h1>
+
+    </div>
+
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
+
